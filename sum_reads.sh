8ea5d92afb9716622a95b317ef98719a2395a075
Jimmy O'Donnell <pearljim3@hotmail.com>
2015-01-12 16:28:03 -0800

    updated sum_reads to count sequences following equals sign

--- sum_reads.sh
+++ sum_reads.sh
@@ -2,17 +2,28 @@
 
 # This script will search all the subdirectories of MY_DIR for any file named TARGET_FILE, and find and return the occurrences of "size=" to the end of the line. It will output the results to a file specified by OUTFILE, which can be modified to show the breakdown of the number of reads per OTU in each file.
 
-MY_DIR='/Users/threeprime/Documents/Data/IlluminaData/12S/run_20140930/Analysis_20141016_1744/demultiplexed'
-TARGET_FILE='6_nosingle.fasta'
-OUTFILE='/Users/threeprime/Desktop/outfile.txt'
+MY_DIR='/Users/threeprime/Documents/GoogleDrive/Illumina_Data/12S/run_20140930/Analysis_20141028_1812_JP/demultiplexed'
+TARGET_FILE='5_derep.fasta'
+OUTFILE='/Users/threeprime/Desktop/outfile.csv'
 
-FILES=$( find "${DIRECTORY}" -type f -name "${FILENAME}" )
+FILES=$( find "${MY_DIR}" -type f -name "${TARGET_FILE}" )
 
+# write the header line of the csv file
+echo "file","sequences" > $OUTFILE
+
+# for each infile, calculate the total number of sequences, and print it along with the shortened file path (everything after the demultiplexed directory)
 for INFILE in $FILES; do
-	grep -o -E -H 'size=.*$' "${INFILE}" >> "${OUTFILE}"
+	SUM=$( awk -F'=' ' />/ { sub(/;/, "", $NF); print $2 } ' "$INFILE" | awk '{ sum+=$1} END {print sum}' )
+	echo "${INFILE#"${MY_DIR}"/}",$SUM >> $OUTFILE
 done
 
-sed 's|'"$DIRECTORY"'||g' "${OUTFILE}" > "${OUTFILE%.*}"_tmp.txt
-sed 's|:|,|g' "${OUTFILE%.*}"_tmp.txt > "${OUTFILE}"
-rm "${OUTFILE%.*}"_tmp.txt
-# awk -F';' ' />/ { print $2 } ' "${INFILE}"
+# Alt, slower
+# awk -F'=' ' />/ { print $2 } ' "$INFILE" | sed 's/;//g' > $OUTFILE
+
+# The old, bad, slow way
+# for INFILE in $FILES; do
+# 	grep -o -E -H 'size=.*$' "${INFILE}" >> "${OUTFILE}"
+# done
+# sed 's|'"$MY_DIR"'||g' "${OUTFILE}" > "${OUTFILE%.*}"_tmp.txt
+# sed 's|:|,|g' "${OUTFILE%.*}"_tmp.txt > "${OUTFILE}"
+# rm "${OUTFILE%.*}"_tmp.txt
