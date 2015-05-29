1d7537baf6f78a007d9109ad78d2284b69f5a323
Jimmy O'Donnell <pearljim3@hotmail.com>
2015-05-28 18:02:36 -0700

    added alternate fasta unzipper (takes directory as argument)

--- misc_operations/count_sequences.sh
+++ misc_operations/count_sequences.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # usage: bash count_sequences.sh '/Path/to/directory' > outputfile.txt
 args=("$@")
---
+++ misc_operations/unzip_fasta.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# usage: bash count_sequences.sh '/Path/to/directory' > outputfile.txt
+args=("$@")
+
+find "${args[0]}" -type f -name '*.fasta.gz' -exec gunzip "{}" \;
+
+# files=$(find "${args[0]}" -type f -name '*.fasta.gz' )
