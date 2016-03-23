f6aedbcb4f1223197b7fdb757906d0f51d3ddceb
Jimmy O'Donnell <jodonnellbio@gmail.com>
2016-03-22 18:42:49 -0700

    modify script to count primer index mismatch

--- beta/tag_counts.R
+++ beta/tag_counts.R
@@ -11,12 +11,65 @@ head(tag_counts)
 # samples with fewer than this proportion of the mean number of reads will be excluded
 lower_percent_threshold <- 0.05
 
+# low_frequency_samples <- which(tag_counts$left_tagged < mean(tag_counts$left_tagged)*lower_percent_threshold)
+# low_frequency_data <- tag_counts[low_frequency_samples, ]
+# tag_counts <- tag_counts[-low_frequency_samples, ]
 
-low_frequency_samples <- which(tag_counts$left_tagged < mean(tag_counts$left_tagged)*lower_percent_threshold)
+boxplot(split(tag_counts$left_tagged, tag_counts$library))
 
-low_frequency_data <- tag_counts[low_frequency_samples, ]
+plot(sort(tag_counts$right_tagged_by_max_v))
 
-tag_counts <- tag_counts[-low_frequency_samples,]
+plot(diff(sort(tag_counts$right_tagged_by_max_v)))
+# BINGO!
+
+
+
+
+left_tagged_by_max <- lapply(split(tag_counts$left_tagged, tag_counts$library), function(x) x/max(x))
+right_tagged_by_max <- lapply(split(tag_counts$right_tagged, tag_counts$library), function(x) x/max(x))
+
+k_membership_l <- unlist(lapply(left_tagged_by_max, function(x) kmeans(x, centers = 2)$cluster))
+k_membership_r <- unlist(lapply(right_tagged_by_max, function(x) kmeans(x, centers = 2)$cluster))
+
+left_tagged_by_max_v <- unlist(left_tagged_by_max)
+right_tagged_by_max_v <- unlist(right_tagged_by_max)
+
+tag_counts <- data.frame(tag_counts, left_tagged_by_max_v, right_tagged_by_max_v, k_membership_l, k_membership_r, stringsAsFactors = FALSE)
+length(k_membership_l)
+nrow(tag_counts)
+
+split(right_tagged_by_max_v, c(tag_counts$library, k_membership_r))
+membership <- 
+tag_counts$right_tagged[k_membership_r == 1,]
+split(, )
+
+{
+}
+
+abundance_by_lib_k <- list()
+for(i in 1:max(k_membership_r)){
+	abundance_by_lib_k[[i]] <- 	lapply(split(tag_counts, tag_counts[,"library"]), function(x) x[x[,"k_membership_r"] == i, "right_tagged_by_max_v"])
+}
+
+
+
+stripchart(
+	abundance_by_lib_k[[1]], 
+	las = 1, 
+	method = "jitter", 
+	pch = 21, 
+	col = 3, 
+	ylab = "library"
+	)
+stripchart(
+	abundance_by_lib_k[[2]], 
+	las = 1, 
+	ylab = "library", 
+	method = "jitter", 
+	pch = 21, 
+	col = 4, 
+	add = TRUE
+	)
 
 
 
