d716d249cce8dff82f19a38e30b74af2a4c9a013
Jimmy O'Donnell <jodonnellbio@gmail.com>
2016-10-24 21:22:18 -0700

    remove blast from banzai

--- banzai.sh
+++ banzai.sh
@@ -913,36 +913,6 @@ else
 
 fi
 
-################################################################################
-# TAXONOMIC ANNOTATION
-################################################################################
-if [ "$PERFORM_BLAST" = "YES" ]; then
-	echo $(date +%Y-%m-%d\ %H:%M) "BLASTing..."
-	blast_output="${DEREP_INPUT%/*}"/10_BLASTed.xml
-	blastn \
-		-query "${BLAST_INPUT}" \
-		-db "$BLAST_DB" \
-		-num_threads "$n_cores" \
-		-perc_identity "${PERCENT_IDENTITY}" \
-		-word_size "${WORD_SIZE}" \
-		-evalue "${EVALUE}" \
-		-max_target_seqs "${MAXIMUM_MATCHES}" \
-		-culling_limit "${culling_limit}" \
-		-outfmt 5 \
-		-out "${blast_output}"
-
-	# check for blast output
-	if [[ ! -s "${blast_output}"  ]]; then
-		echo
-		echo 'BLAST failed: the output file is empty or absent.'
-	    echo 'File should be:' "${blast_output}"
-		echo
-	fi
-else
-	echo 'BLAST search not performed.'
-	echo
-fi
-
 
 if [ "$PERFORM_CLEANUP" = "YES" ]; then
 	echo $(date +%Y-%m-%d\ %H:%M) "Compressing fasta, fastq, and xml files..."
