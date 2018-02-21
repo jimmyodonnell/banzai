cb3c081a13bfdf5505d0fc69fa643c6ae44feb8c
Jimmy O'Donnell <jodonnellbio@gmail.com>
2018-02-21 15:48:36 -0800

    add function to count sequences

---
+++ scripts/functions/count_seq.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# count the sequences in a fastq or fasta file
+# author: Jimmy O'Donnell
+# usage: count_seq /path/to/a/file.fastq
+
+count_seq () {
+  # detect file type by first character
+  CHAR1=$( head -n 1 "${1}" | cut -c-1 )
+  if [ "${CHAR1}" == '@' ]; then
+    FILETYPE='fastq'
+  elif [ "${CHAR1}" == '>' ]; then
+    FILETYPE='fasta'
+  else
+    echo 'unable to properly detect file type'
+    return 1
+  fi
+  
+  if [ "${FILETYPE}" == 'fasta' ]; then
+    seq_count=$( grep -c '^>' "${1}" )
+  else
+    fastq_lines=$( cat merged_pear.fastq.assembled.fastq | wc -l )
+    seq_count=$(echo "${fastq_lines}" / 4 | bc)
+  fi
+  echo $seq_count
+  return 0
+}
