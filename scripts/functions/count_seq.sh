18a97e983543ffb17e2bf57896ad12ec755adb37
Jimmy O'Donnell <jodonnellbio@gmail.com>
2018-02-21 16:35:54 -0800

    allow compressed input

--- scripts/functions/count_seq.sh
+++ scripts/functions/count_seq.sh
@@ -5,8 +5,17 @@
 # usage: count_seq /path/to/a/file.fastq
 
 count_seq () {
+  # test for gzip compression
+  is_compressed=$( file "${1}" | grep -c 'gzip')
+
   # detect file type by first character
-  CHAR1=$( head -n 1 "${1}" | cut -c-1 )
+  if [ "${is_compressed}" = 1 ]; then
+    CHAR1=$( gunzip -c "${1}" | head -n 1 | cut -c-1 )
+  else
+    CHAR1=$( head -n 1 "${1}" | cut -c-1 )
+  fi
+  
+  # set filetype
   if [ "${CHAR1}" == '@' ]; then
     FILETYPE='fastq'
   elif [ "${CHAR1}" == '>' ]; then
@@ -17,9 +26,17 @@ count_seq () {
   fi
   
   if [ "${FILETYPE}" == 'fasta' ]; then
-    seq_count=$( grep -c '^>' "${1}" )
+    if [ "${is_compressed}" = 1 ]; then
+      seq_count=$( gunzip -c "${1}" | grep -c '^>' )
+    else
+      seq_count=$( grep -c '^>' "${1}" )
+    fi
   else
-    fastq_lines=$( cat "${1}" | wc -l )
+    if [ "${is_compressed}" = 1 ]; then
+      fastq_lines=$( gunzip -c "${1}" | wc -l )
+    else
+      fastq_lines=$( cat "${1}" | wc -l )
+    fi
     seq_count=$(echo "${fastq_lines}" / 4 | bc)
   fi
   echo $seq_count
